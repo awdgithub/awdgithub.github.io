d3103684a8a08dfe0c57327b886585f7771cd8da
awdgithub <130380221+awdgithub@users.noreply.github.com>
2023-08-09 20:21:08 -0400

    Update scrapes.py

--- scrapes.py
+++ scrapes.py
@@ -1,1 +1,4 @@
-
+from selenium import webdriver
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+absenteeism = [] #Data on absenteeism
+driver.get("<a href="https://schoolquality.virginia.gov/schools/meadowbrook-high#fndtn-desktopTabs-climate">)
